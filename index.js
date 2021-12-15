21e1ea816fd456bfeafba6c2cf42c4e4f2ff215d
Benji Tusk <benjitusk1@gmail.com>
2021-12-15 19:59:18 +0200

    changed chat polls are sent to

--- index.js
+++ index.js
@@ -476,7 +476,7 @@ bot.client.on('message_create', async msg => {
       }
       if (!(options.id || options.type || options.topic)) return msg.reply("Sorry, but you are missing some parameters. The correct syntax is\n```!poll```\n```id: <pollID>```\n```type: <pollType>```\n```topic: <pollTopic>```\n```[test: <true/false>]```\n\n_Pro tip: A test poll privately sends you a preview of the poll, so you can test it out before sending it to the group._");
       msg.reply(`Sending a poll with the following options:\n\n${JSON.stringify(options, null, 2)}`);
-      let chat = options.test ? msg.chat : await bot.client.getChatById(redacted.TEST_CHAT_ID);
+      let chat = options.test ? msg.chat : await bot.client.getChatById(redacted.BOT_MAIN_CHAT);
       pollManager.publish(options.id, chat, options.type, options.topic, options.test);
     }
     switch (msg.body) {
