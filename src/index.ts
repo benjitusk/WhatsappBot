586cbbcd002f50ebd8e31eddbb66fdab9c28b9d1
Benji Tusk <benjitusk1@gmail.com>
2022-01-09 23:49:27 +0200

    updated handling nonexsistant session

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import { Client } from 'whatsapp-web.js';
-
+import { existsSync } from 'fs';
+const sessionPath = '../sessions/bot.json';
 const client = new Client({
-	/*     puppeteer: {
-        headless: false,
-    }, */
-	session: require('../sessions/bot.json'),
+	puppeteer: {
+		headless: false,
+	},
+	session: existsSync(sessionPath) ? require(sessionPath) : undefined,
 });
 
 client.initialize();
 
-['event', 'commands'].forEach((handler) => {
+['event'].forEach((handler) => {
 	require(`./handlers/${handler}`)(client);
 });
