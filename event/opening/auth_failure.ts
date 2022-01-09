533259f029617a06b08f3e6292885fee47915529
Benji Tusk <benjitusk1@gmail.com>
2022-01-09 14:26:57 +0200

    Added auth_failure event handler

---
+++ event/opening/auth_failure.ts
@@ -0,0 +1,10 @@
+import { Client } from 'whatsapp-web.js';
+
+module.exports = {
+  name: 'auth_failure',
+  once: true,
+  async execute(error: any, client: Client) {
+    console.log('[Client] Authentication failed');
+    console.log(error);
+  },
+};
