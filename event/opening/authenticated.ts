dad3ef5b1091fe0947166fb994e67acc8ac2f71f
Benji Tusk <benjitusk1@gmail.com>
2022-01-09 14:41:38 +0200

    added authenticated event handler

---
+++ event/opening/authenticated.ts
@@ -0,0 +1,13 @@
+import { Client, ClientSession } from 'whatsapp-web.js';
+import { writeFile } from 'fs';
+module.exports = {
+  name: 'authenticated',
+  once: true,
+  async execute(session: ClientSession, client: Client) {
+    let sessionString = JSON.stringify(session);
+    writeFile('../../sessions/bot.js', sessionString, (err) => {
+      if (err) console.error(err);
+    });
+    console.log('[Client] Authenticated');
+  },
+};
