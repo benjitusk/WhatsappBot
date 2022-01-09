287fdb25e3602a6fef7f9d2abe959add43f9b412
Benji Tusk <benjitusk1@gmail.com>
2022-01-09 15:10:38 +0200

    Whitespace changeover

--- event/opening/qr.ts
+++ event/opening/qr.ts
@@ -2,10 +2,10 @@ import qrcode from 'qrcode-terminal';
 import { Client } from 'whatsapp-web.js';
 
 module.exports = {
-  name: 'qr',
-  once: true,
-  async execute(qr: String, client: Client) {
-    console.log('[Client] Received QR code');
-    qrcode.generate(qr, { small: true });
-  },
+	name: 'qr',
+	once: true,
+	async execute(qr: String, client: Client) {
+		console.log('[Client] Received QR code');
+		qrcode.generate(qr, { small: true });
+	},
 };
