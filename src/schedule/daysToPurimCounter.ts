b84d0c8ef10b63691d69e518afa6d52e43e812b4
Benji Tusk <benjitusk1@gmail.com>
2022-01-11 00:29:09 +0200

    added DaysToPurim counter task

---
+++ src/schedule/daysToPurimCounter.ts
@@ -0,0 +1,21 @@
+import { PersistantStorage } from '../utils';
+import { Task } from '../utils';
+
+const task: Task = {
+	name: 'DTP Countdown',
+	enabled: true,
+	seconds: '0',
+	minutes: '0',
+	hours: '18',
+	dayMonth: '*',
+	month: '*',
+	dayWeek: '*',
+	execute: async function () {
+		const persistantStorage = new PersistantStorage();
+		let dtp = persistantStorage.get('daysToPurim');
+		dtp--;
+		persistantStorage.set('daysToPurim', dtp);
+	},
+};
+
+module.exports = task;
