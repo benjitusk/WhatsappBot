a5a9c65f5a8bf9078b0750b849f1412e774e2f49
Benji Tusk <benjitusk1@gmail.com>
2022-01-10 16:00:34 +0200

    Added logging for scheduled messages

--- src/handlers/schedule.ts
+++ src/handlers/schedule.ts
@@ -2,16 +2,40 @@ import { Client } from 'whatsapp-web.js';
 import { readdirSync } from 'fs';
 import { CronJob } from 'cron';
 
+interface Task {
+	name: string;
+	enabled: any;
+	seconds: any;
+	minutes: any;
+	hours: any;
+	dayMonth: any;
+	month: any;
+	dayWeek: any;
+	execute: (client: Client) => void;
+}
+
 module.exports = (client: Client): void => {
 	// Get all js files in the folder. These files are the event handlers.
 	const eventFiles = readdirSync(`./schedule`).filter((file) => file.endsWith('.js'));
 	for (const file of eventFiles) {
 		// Load the event handler.
-		const task = require(`../schedule/${file}`);
+		const task: Task = require(`../schedule/${file}`);
 		if (task.enabled) {
 			// Create a new cron job
 			let timeString = `${task.seconds} ${task.minutes} ${task.hours} ${task.dayMonth} ${task.month} ${task.dayWeek}`;
-			new CronJob(timeString, task.execute.bind(null, client), null, true, 'Asia/Jerusalem');
+			new CronJob(timeString, generateTaskFunction(task, client), null, true, 'Asia/Jerusalem');
 		}
 	}
 };
+
+function generateTaskFunction(task: Task, client: Client): () => void {
+	return function () {
+		console.log('[Schedule] Executing task: ' + task.name);
+		try {
+			task.execute(client);
+		} catch (e) {
+			console.error(`[Schedule] Error executing task: ${task.name}`);
+			console.error(e);
+		}
+	};
+}
