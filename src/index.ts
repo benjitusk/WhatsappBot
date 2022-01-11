f95af6047cd863c6be5f54d2788326ebbef67a3d
Benji Tusk <benjitusk1@gmail.com>
2022-01-11 21:57:19 +0200

    feat: Log on process start and stop

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Client } from 'whatsapp-web.js';
 import { existsSync } from 'fs';
 import { Collection } from '@discordjs/collection';
+
+// Log to the console that we are starting execution
+console.log('\n=== [START] ===');
+
 const sessionPath = '../sessions/bot.json';
 const client = new Client({
 	// puppeteer: {
@@ -16,4 +20,9 @@ client.initialize();
 
 ['events', 'schedules', 'commands'].forEach((handler) => {
 	require(`./handlers/${handler}`)(client);
+});
+
+// Log to the console on process exit
+process.on('exit', () => {
+	console.log('=== [STOP] ===\n');
 });
