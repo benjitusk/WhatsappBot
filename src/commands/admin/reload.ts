0dfb0e235d68860199fc7ede6d42d0d8f4f88842
Benji Tusk <benjitusk1@gmail.com>
2022-01-12 14:43:22 +0200

    feat: ugraded reload command

--- src/commands/admin/reload.ts
+++ src/commands/admin/reload.ts
@@ -1,4 +1,4 @@
-import { Message } from 'whatsapp-web.js';
+import { Client, Message } from 'whatsapp-web.js';
 import { Command } from '../../types';
 import { PersistantStorage } from '../../utils';
 
@@ -8,9 +8,24 @@ const command: Command = {
 	admin: true,
 	aliases: ['load'],
 	cooldown: 0,
-	execute: function (message: Message): void {
-		new PersistantStorage().load();
-		message.reply('Reloaded!');
+	execute: function (message: Message, client: Client, args: string[] = []): void {
+		let operation: string;
+		args.length > 1 ? (operation = args[1]) : (operation = 'storage');
+		switch (operation) {
+			case 'storage':
+				new PersistantStorage().load();
+				console.log('[STORAGE] reloaded');
+				break;
+			case 'commands':
+				require('../../handlers/commands')(client);
+				console.log('[COMMANDS] reloaded');
+				break;
+			case 'schedules':
+				require('../../handlers/schedules')(client);
+				console.log('[SCHEDULES] reloaded');
+				break;
+		}
+		message.reply(`Reloaded ${operation}.`);
 	},
 };
 
