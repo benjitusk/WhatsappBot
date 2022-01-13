9f7380299347bb4da6e6999d63e3631f77711b31
Benji Tusk <benjitusk1@gmail.com>
2022-01-13 17:03:33 +0200

    feat: added leib command

---
+++ src/commands/misc/leib.ts
@@ -0,0 +1,16 @@
+import { Message } from 'whatsapp-web.js';
+import { Command } from '../../types';
+
+const command: Command = {
+	name: 'leib',
+	enabled: false,
+	admin: false,
+	aliases: [],
+	cooldown: 0,
+	execute: function (message: Message): void {
+		// Pick a random emoji from the following: 🥳 👏 🇦🇺
+		const emojis = ['🥳', '👏', '🇦🇺'];
+		const emoji = emojis[Math.floor(Math.random() * emojis.length)];
+		message.reply(emoji);
+	},
+};
