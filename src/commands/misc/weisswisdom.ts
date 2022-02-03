f0caecbab788c3f305818b733e4d74fbe83fec38
BenjiTusk <benjitusk1@gmail.com>
2022-02-03 19:26:47 +0200

    fix(commands): added funny disabled message

--- src/commands/misc/weisswisdom.ts
+++ src/commands/misc/weisswisdom.ts
@@ -1,28 +1,22 @@
 import { Message, Client } from 'whatsapp-web.js';
 import { Command } from '../../types';
-import { PersistantStorage } from '../../utils';
 
 const command: Command = {
 	name: 'weisswisdom',
 	helpText: 'Get a random quote from the Ami Weiss Wisdom Database (TM)',
 	syntax: 'weisswisdom',
-	enabled: false,
+	enabled: true,
 	admin: false,
 	aliases: ['amiadvice', 'amiquote'],
-	cooldown: 60 * 60 * 6, // 6 hour cooldown
+	cooldown: 0,
 	execute: async function (
 		message: Message,
 		client: Client,
 		args: string[]
 	): Promise<void> {
-		// load persistent storage
-		const persistance = new PersistantStorage();
-		const storage = persistance.get();
-		// get a random quote
-		const quote =
-			storage.amiQuotes[Math.floor(Math.random() * storage.amiQuotes.length)]
-				.quote;
-		message.reply(`^_^\n\n${quote}`);
+		message.reply(
+			`You need to wait ∞ minutes and ${new Date().getSeconds()} seconds before using this command again`
+		);
 	},
 };
 
