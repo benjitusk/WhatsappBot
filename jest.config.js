8e0de21c60f1a8b84db8ef251b43630024baaf8f
BenjiTusk <benjitusk1@gmail.com>
2022-02-18 10:47:09 +0200

    test: Added testing framework

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    },
+  }
+};
---
+++ src/__tests__/first.test.ts
@@ -0,0 +1,4 @@
+// Create a ts-jest test that will fail
+it('should fail', () => {
+	expect(1).toBe(2);
+});
