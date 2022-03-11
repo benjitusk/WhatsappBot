76b3239f9b68d0f90aa9ba725f070f1c270c7b7a
BenjiTusk <benjitusk1@gmail.com>
2022-03-11 14:15:40 +0200

    refactor: renamed votekick

--- src/commands/kick/votekick.ts
+++ src/commands/kick/votekick.ts
@@ -8,20 +8,20 @@ const command: Command = {
 	helpText: 'Initiate a votekick to remove the mentioned user',
 	syntax: '!votekick @contact',
 	enabled: true,
-	admin: true,
+	admin: false,
 	aliases: [],
-	cooldown: 60 * 60 * 12, // 12 hours
+	cooldown: 0, // 0 hours
 	execute: async function (message: Message, client: Client, args: string[]): Promise<void> {
 		// Check if a user was @mentioned
 		const mentionedUser = await message.getMentions();
 		if (mentionedUser.length !== 1) {
 			message.reply(
-				`Please @mention exactly 1 user to kick. You must wait ${prettyMilliseconds(
+				`Please @mention exactly 1 user to votekick. You must wait ${prettyMilliseconds(
 					this.cooldown * 1000,
 					{
 						secondsDecimalDigits: 0,
 					}
-				)} hours before you can use this command again.`
+				)} before you can use this command again.`
 			);
 			return;
 		}
@@ -33,8 +33,17 @@ const command: Command = {
 		// Make sure the user is not an admin on the chat
 		// Get chat admins
 		const chat = (await message.getChat()) as GroupChat;
-		if (!chat.isGroup) return;
-		for (const user of chat.participants) if (user.isAdmin) return;
+		if (!chat.isGroup) {
+			console.log(`${chat.name} is not a group, skipping...`);
+		}
+		for (const user of chat.participants)
+			if (user.id._serialized === userContactId) {
+				if (user.isAdmin) {
+					console.log(`${userContact.number} is an admin on ${chat.name}, skipping...`);
+					return;
+				}
+				break;
+			}
 
 		if (Users.shared.userHasActiveVoteKick(userContactId, chat.id._serialized)) {
 			message.reply(`A VoteKick is currently in progress for that user.`);
@@ -46,8 +55,11 @@ const command: Command = {
 			chat.id._serialized,
 			message.author!
 		)!;
+		Users.shared.voteKickVote(message.author!, voteKickID, client);
 		message.reply(
-			`@${userContact.number}'s fate is being decided... If 5 replies with "!kick" are received within 7 minutes, the user will be kicked.` +
+			`@${userContact.number}'s fate is being decided... If ${
+				Users.VOTEKICKCOUNT - 1
+			} replies with "!🥾" are received within 7 minutes, the user will be kicked.` +
 				`\n\n*Votes that are not direct replies to this message will be ignored*\n\nVoteKickID: ${voteKickID}`,
 			undefined,
 			{
