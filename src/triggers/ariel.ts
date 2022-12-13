d07bea698508035a808dd67413d650147ffd3e04
BenjiTusk <benjitusk1@gmail.com>
2022-12-13 20:43:11 +0200

    feat: Added autoresponse

---
+++ src/triggers/ariel.ts
@@ -0,0 +1,28 @@
+import { Client, Message } from 'whatsapp-web.js';
+import { Contacts } from '../removedInfo';
+import { AutoResponse } from '../types';
+
+const AutoReply: AutoResponse = {
+	name: 'Ariel PregMan',
+	enabled: true,
+	execute: async function (message: Message, client: Client): Promise<void> {
+		let pregnantMen = ['🫃', '🫃🏻', '🫃🏼', '🫃🏽', '🫃🏾', '🫃🏿'];
+		let pregManIndex = Math.floor(Math.random() * pregnantMen.length);
+		let pregMan = pregnantMen[pregManIndex];
+		message.react(pregMan);
+	},
+	executeCondition: async function (message: Message): Promise<boolean> {
+		if (message.author == Contacts.ARIEL_BLUMSTEIN) {
+			await delay(Math.random() * 4000 + 3000); // between 3k and 7k ms
+			return Promise.resolve(true);
+		} else return Promise.resolve(false);
+	},
+};
+
+function delay(ms: number) {
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, ms);
+	});
+}
+
+module.exports = AutoReply;
